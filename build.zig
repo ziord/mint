b21268c4bf73690574b88e1615454786a77a46dd
ziord <jeremiahikosin@gmail.com>
2026-06-17 19:11:08 -0400

    update exe name

--- build.zig
+++ build.zig
@@ -58,7 +58,7 @@ pub fn build(b: *std.Build) void {
     // If neither case applies to you, feel free to delete the declaration you
     // don't need and to put everything under a single module.
     const exe = b.addExecutable(.{
-        .name = "zinc",
+        .name = "mint",
         .root_module = b.createModule(.{
             // b.createModule defines a new module just like b.addModule but,
             // unlike b.addModule, it does not expose the module to consumers of
